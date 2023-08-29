58a42ae4e6b4f2791aba3732ad0052b5a9f499dc
Guilherme Correia <guicorrea384@gmail.com>
2023-08-29 11:50:00 -0300

    ♻️ Turn Home into a functional component

--- src/Home.tsx
+++ src/Home.tsx
@@ -1,19 +1,9 @@
-import Nullstack from 'nullstack'
-
 import NinjaMode from './components/NinjaMode'
 import Tasks from './components/Tasks'
 
-class Home extends Nullstack {
-
-  render() {
-    return (
-      <>
-        <NinjaMode key="ninja" />
-        <Tasks key="tasks" />
-      </>
-    )
-  }
-
-}
-
-export default Home
+export default () => (
+  <>
+    <NinjaMode key="ninja" />
+    <Tasks key="tasks" />
+  </>
+)
